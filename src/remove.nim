1c42bcb64bd8a24ca99834922eef4f392e215cb6
leejoker <1056650571@qq.com>
2023-08-29 11:08:08 +0800

    fix remove bug

--- src/remove.nim
+++ src/remove.nim
@@ -2,6 +2,7 @@ import cmd
 import jpvm_utils
 import os
 import strformat
+import global
 
 const REMOVE_HELP_USAGE = "remove [distro] [version]"
 const REMOVE_HELP_COMMENT = "移除指定版本"
@@ -9,7 +10,7 @@ const REMOVE_HELP_COMMENT = "移除指定版本"
 proc removeProc(command: CommandLine) =
   var (distro, version, path) = jdkVersionPath(command.optArguments)
   if path != "":
-    removeDir(path)
+    removeDir(joinPath(jdkPath, distro, version))
     echo fmt"已移除版本 {distro}-{version}"
 
 
